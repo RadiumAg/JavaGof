190535bcac6a255ed91f1562167baa0c786e58a2
Radium <2043869032@qq.com>
2021-02-04 10:58:48 +0800

    提交

--- JavaGof/src/com/company/DataStructure/Graph/Graph.java
+++ JavaGof/src/com/company/DataStructure/Graph/Graph.java
@@ -1,61 +1,102 @@
 package com.company.DataStructure.Graph;
+
 // 图
 public class Graph {
-    private  final int MAX_VERTS = 20;
-    private  Vertex vertexList[]; // 节点列表
-    private  int adjMat[][]; // 邻接矩阵
-    private  int nVerts; // 当前的节点索引
+    private final int MAX_VERTS = 20;
+    private Vertex vertexList[]; // 节点列表
+    private int adjMat[][]; // 邻接矩阵
+    private int nVerts; // 当前的节点索引
+    private StackX theStack;
 
-    public  Graph(){
+    public Graph() {
+        theStack = new StackX();
         vertexList = new Vertex[MAX_VERTS];
         adjMat = new int[MAX_VERTS][MAX_VERTS];
         nVerts = 0;
-        for (int i = 0; i < MAX_VERTS; i++) {
-            for (int j = 0; j < MAX_VERTS; j++) {
+        for (int j = 0; j < MAX_VERTS; j++) {
+            for (int k = 0; k < MAX_VERTS; k++) {
                 adjMat[j][k] = 0;
             }
         }
     }
 
-    public void dfs(){
+    public void dfs() {
         vertexList[0].wasVisited = true;
         displayVertex(0);
+        this.theStack.push(0);
 
-    }
-
-    public  int getAdjUnvisitedVertex(int v){
-        for (int i = 0; i < nVerts; i++) {
-            if (adjMat[v][j] === 1 && vertexList[j].wasVisited == false) return j;
-            return  -1;
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(theStack.peek());
+            if (v == -1)
+                theStack.pop();
+            else {
+                this.vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
         }
+
+        for (int j = 0; j < nVerts; j++) {
+            vertexList[j].wasVisited = false;
+        }
+
     }
-    public  void addVertex(char lab)
-    {
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int j = 0; j < nVerts; j++) {
+            if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) return j;
+        }
+        return -1;
+    }
+
+    public void addVertex(char lab) {
         vertexList[nVerts++] = new Vertex(lab);
     }
 
-    public  void  addEdge(int start, int end){
+    public void addEdge(int start, int end) {
         adjMat[start][end] = 1;
         adjMat[end][start] = 1;
     }
 
-    public  void  displayVertex(int v){
+    public void displayVertex(int v) {
         System.out.print(vertexList[v].label);
     }
 }
 
 // 顶点
-class  Vertex {
-    public  char label;
-    public  boolean wasVisited;
+class Vertex {
+    public char label;
+    public boolean wasVisited;
 
-    public Vertex(char lab ){
+    public Vertex(char lab) {
         label = lab;
         wasVisited = false;
     }
 }
 
 class StackX {
-    private  final  int SIZE = 20;
-    private  int[] st;
+    private final int SIZE = 20;
+    private int[] st;
+    private int top;
+
+    public StackX() {
+        st = new int[SIZE];
+        this.top = -1;
+    }
+
+    public void push(int j) {
+        st[++top] = j;
+    }
+
+    public int pop() {
+        return st[this.top--];
+    }
+
+    public int peek() {
+        return st[this.top];
+    }
+
+    public boolean isEmpty() {
+        return (this.top == -1);
+    }
 }
---
+++ JavaGof/src/com/company/JavaGof/Main.java
@@ -0,0 +1,37 @@
+package com.company.JavaGof;
+import com.company.DataStructure.Graph.Graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        var redCircle = new Circle(100, 100, 10, new RedCircle());
+//        var greenCircle = new Circle(100, 100, 10, new GreenCircle());
+//
+//        redCircle.draw();
+//        greenCircle.draw();
+//
+//        var chRemote = new self.my.Remote(new ChangHongTv());
+//        var aocRemote = new self.my.Remote(new AOCTv());
+//        chRemote.open();
+//        aocRemote.open();
+//        int[] a = new int[3];
+//        Arrays.stream(a).map(x->2);
+
+//        Graph theGraph = new Graph();
+//        theGraph.addVertex('A');
+//        theGraph.addVertex('B');
+//        theGraph.addVertex('C');
+//        theGraph.addVertex('D');
+//        theGraph.addVertex('E');
+//        theGraph.addEdge(0,1); // AB
+//        theGraph.addEdge(1,2); //BC
+//        theGraph.addEdge(0,3); //AD
+//        theGraph.addEdge(3,4); //DE
+//        System.out.print("Visits:");
+//        theGraph.dfs();
+//        System.out.print("");
+    }
+}
+
+
+
