d1283e4ec5b18bdc711951603d905e45ac7dcd00
Radium <2043869032@qq.com>
2021-03-02 17:48:58 +0800

    增加最小生成树

---
+++ JavaGof/src/com/company/JavaGof/Mediator/Mediator.java
@@ -0,0 +1,7 @@
+package com.company.JavaGof.Mediator;
+
+public abstract class Mediator {
+     public  abstract  void  register(Colleague colleague);
+
+     public  abstract  void  relay(Colleague cl);
+}
