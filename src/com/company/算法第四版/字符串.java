38e8e8cdfeede2849902b43cf8b13de56c6f09ca
Radium <2043869032@qq.com>
2022-01-23 22:42:57 +0800

    Default Changelist

--- src/com/company/算法第四版/字符串.java
+++ src/com/company/算法第四版/字符串.java
@@ -2,7 +2,7 @@ package com.company.算法第四版;
 
 import edu.princeton.cs.algs4.Insertion;
 
-import java.util.SortedMap;
+import java.util.Comparator;
 
 public class 字符串 {
 
@@ -52,48 +52,82 @@ class LSD {
     }
 }
 
-///**
-// * 高位优先的字符串排序
-// */
-//class MSD {
-//    private static int R = 256;       // 基数
-//    private static final int M = 15;  // 小数组的切换阈值
-//    private static String[] aux;      // 数据分类的辅助数组
-//
-//    private static int charAt(String s, int d) {
-//        if (d < s.length()) return s.charAt(d);
-//        else return -1;
-//    }
-//
-//    public static void srot(String[] a) {
-//        int N = a.length;
-//        aux = new String[N];
-//        sort(a, 0, N - 1, 0);
-//    }
-//
-//}
-//    private static void sort(String[] a, int lo, int hi, int d) {
-//        if (hi <= lo + M) {
-//            Insertion.sort(a, lo, hi, d);
-//            return;
-//        }
-//        int[] count = new int[R + 2];
-//        for (int i = lo; i <= hi; i++) {
-//            count[charAt(a[i], d) + 2]++;
-//        }
-//
-//        for (int r = 0; r < R + 1; r++)
-//            count[r + 1] += count[r];
-//
-//        for (int i = lo; i <= hi; i++)
-//            aux[count[charAt(a[i], d) + 1]++] = a[i];
-//
-//        for (int i = lo; i <= hi; i++)
-//            a[i] = aux[i - lo];
-//
-//        for (int r = 0; r < R; r++)
-//            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
-//
-//    }
+/**
+ * 高位优先的字符串排序
+ */
+class MSD {
+    private static int R = 256;       // 基数
+    private static final int M = 15;  // 小数组的切换阈值
+    private static String[] aux;      // 数据分类的辅助数组
 
-//}
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        else return -1;
+    }
+
+    public static void srot(String[] a) {
+        int N = a.length;
+        aux = new String[N];
+        sort(a, 0, N - 1, 0);
+    }
+
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo + M) {
+            Insertion.sort(a, lo, hi, new Comparator() {
+                @Override
+                public int compare(Object o1, Object o2) {
+                    return d;
+                }
+            });
+            return;
+        }
+        int[] count = new int[R + 2];
+        for (int i = lo; i <= hi; i++) {
+            count[charAt(a[i], d) + 2]++;
+        }
+
+        for (int r = 0; r < R + 1; r++)
+            count[r + 1] += count[r];
+
+        for (int i = lo; i <= hi; i++)
+            aux[count[charAt(a[i], d) + 1]++] = a[i];
+
+        for (int i = lo; i <= hi; i++)
+            a[i] = aux[i - lo];
+
+        for (int r = 0; r < R; r++)
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+    }
+}
+
+
+class Quick3string {
+    private static int charAt(String s, int d) {
+        if (d < s.length()) return s.charAt(d);
+        else return -1;
+    }
+
+    public static void sort(String[] a) {
+        sort(a, 0, a.length - 1, 0);
+    }
+
+    private static void exch(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (hi <= lo) return;
+        int lt = lo, gt = hi;
+        int v = charAt(a[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = charAt(a[i],d);
+            if (t<v) exch(a, lt++,i++);
+            else if(t>v) exch(a, i, gt--);
+            else  i++;
+        }
+        }
+    }
