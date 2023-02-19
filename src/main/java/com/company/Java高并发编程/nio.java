c3cb4f798b37c0cf819d71ded8af597a2e2ac87b
Radium <2043869032@qq.com>
2023-02-19 14:00:49 +0800

    Default Changelist

--- src/main/java/com/company/Java高并发编程/nio.java
+++ src/main/java/com/company/Java高并发编程/nio.java
@@ -3,25 +3,37 @@ package com.company.Java高并发编程;
 import java.nio.IntBuffer;
 
 public class nio {
- static IntBuffer intBuffer = null;
- public  static  void allocateTest(){
-     intBuffer = IntBuffer.allocate(20);
-     System.out.println(intBuffer.position());
-     System.out.println(intBuffer.limit());
-     System.out.println(intBuffer.capacity());
+    static IntBuffer intBuffer = null;
 
-     // 一个整型的Buffer静态变量
+    public static void allocateTest() {
+        intBuffer = IntBuffer.allocate(20);
+        System.out.println(intBuffer.position());
+        System.out.println(intBuffer.limit());
+        System.out.println(intBuffer.capacity());
 
-     for (int i =0; i< 5; i++) {
-        intBuffer.put(i);
-     }
-     System.out.println(intBuffer.position());
-     System.out.println(intBuffer.limit());
-     System.out.println(intBuffer.capacity());
+        // 一个整型的Buffer静态变量
 
-     intBuffer.flip();
-     System.out.println(intBuffer.position());
-     System.out.println(intBuffer.limit());
-     System.out.println(intBuffer.capacity());
- }
+        for (int i = 0; i < 5; i++) {
+            intBuffer.put(i);
+        }
+        System.out.println(intBuffer.position());
+        System.out.println(intBuffer.limit());
+        System.out.println(intBuffer.capacity());
+
+        intBuffer.flip();
+        System.out.println(intBuffer.position());
+        System.out.println(intBuffer.limit());
+        System.out.println(intBuffer.capacity());
+
+
+        for (int i = 0; i < 5; i++) {
+            var r = intBuffer.get();
+            System.out.println(r);
+        }
+        System.out.println(intBuffer.position());
+        System.out.println(intBuffer.limit());
+        System.out.println(intBuffer.capacity());
+
+        // fileChannel
+    }
 }
